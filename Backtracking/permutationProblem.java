892707adf40aeceb81e3924170184fa9bc4e352a
ItsSandeepV7 <ritikverma5738@gmail.com>
2025-12-31 15:17:08 +0530

    Solved permuation problem

---
+++ Backtracking/permutationProblem.java
@@ -0,0 +1,23 @@
+public class permutationProblem {
+
+    public static void findPermutation(String str,String ans_str){
+
+        //Base Case
+        if(str.length()==0){
+            System.out.println(ans_str);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char currChar=str.charAt(i);
+            String updated_str=str.substring(0,i)+str.substring(i+1);// "abcde"="ab+"de"=>abde hence c is removed in this way
+            findPermutation(updated_str, ans_str+currChar);
+        }
+        
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        String ans_str="";
+        findPermutation(str,ans_str);
+    }
+}
